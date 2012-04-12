15946fb45f02b12263aa1d6eb97bb1ab33d9659f
Thomas <matt.morrison@theimtgroup.com>
2012-04-12 11:54:12 -0500

    init

---
+++ one/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sample.one',
+    packages=[
+        'sample.one',
+    ],
+    namespace_packages=[
+        "sample",
+    ]
+)
---
+++ two/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sample.two',
+    packages=[
+        'sample.two',
+    ],
+    namespace_packages=[
+        "sample",
+    ]
+)
